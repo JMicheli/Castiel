fad1f219080b1f1aa43bbd2b5d2a1a4ec2002cd3
Joseph Micheli <josephwmicheli@gmail.com>
2025-05-11 22:09:21 -0500

    Simplify About page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -30,37 +30,33 @@ function About() {
       <h1 className="title is-3">About Castiel</h1>
 
       {/* Description */}
-      <div className="box">
-        <h2 className="subtitle is-5">Description</h2>
+      <div className="block">
         <p className="content">
           Castiel allows you to discover and control Chromecast devices on your
           network.
         </p>
       </div>
 
-      {/* Versions */}
-      <div className="box">
-        {/* Server version */}
-        <div className="block">
-          <h2 className="subtitle is-5">Server Version</h2>
-          {serverVersion ? (
-            <span className="tag is-success is-medium">
-              {serverVersion.version}
-            </span>
-          ) : serverError ? (
-            <div className="notification is-danger is-light">{serverError}</div>
-          ) : (
-            <button className="button is-loading is-white is-inverted" disabled>
-              Loading…
-            </button>
-          )}
-        </div>
+      {/* Server version */}
+      <div className="block">
+        <h2 className="subtitle is-5">Server Version</h2>
+        {serverVersion ? (
+          <span className="tag is-success is-medium">
+            {serverVersion.version}
+          </span>
+        ) : serverError ? (
+          <div className="notification is-danger is-light">{serverError}</div>
+        ) : (
+          <button className="button is-loading is-white is-inverted" disabled>
+            Loading…
+          </button>
+        )}
+      </div>
 
-        {/* Frontend version */}
-        <div className="block">
-          <h2 className="subtitle is-5">Frontend Version</h2>
-          <span className="tag is-info is-medium">{frontendVersion}</span>
-        </div>
+      {/* Frontend version */}
+      <div className="block">
+        <h2 className="subtitle is-5">Frontend Version</h2>
+        <span className="tag is-info is-medium">{frontendVersion}</span>
       </div>
     </>
   );
